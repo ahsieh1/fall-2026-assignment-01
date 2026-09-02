31610c4d0f8177b2cd0067296663074b2f865186
ahsieh1 <ahsieh1@charlotte.edu>
2026-09-02 20:53:09 +0000

    Finished Excercise 11

--- src/exercise11.ts
+++ src/exercise11.ts
@@ -1,4 +1,10 @@
+import fs from 'fs/promises';
+
 export async function logStatusToFile(
   filePath: string,
   statusMessage: string,
-): Promise<void> {}
+): Promise<void> {
+  const timestamp = new Date().toISOString();
+  const logEntry = `${statusMessage} ${timestamp}`;
+  await fs.writeFile(filePath, logEntry, 'utf-8');
+}
