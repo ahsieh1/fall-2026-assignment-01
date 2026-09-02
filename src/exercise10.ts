884b69cb401506309cae19da64682a934f3250df
ahsieh1 <ahsieh1@charlotte.edu>
2026-09-02 20:45:20 +0000

    Finished Excercise 10

--- src/exercise10.ts
+++ src/exercise10.ts
@@ -1,15 +1,42 @@
-type UserAccount = {};
+export type UserAccount = {
+  id: string;
+  createdAt: Date;
+  email: string;
+  passwordHash: string;
+  profile: {
+    bio: string;
+    avatarUrl: string;
+  };
+};
 
 export class UserRegistry {
+  private users: Record<string, UserAccount> = {};
+
   public registerUser(
     data: Omit<UserAccount, 'id' | 'createdAt'>,
   ): UserAccount {
-    return {};
+    const newUser: UserAccount = {
+      ...data, id: Math.random().toString(36).substring(2, 10), createdAt: new Date(),
+    };
+    
+    this.users[newUser.id] = newUser;
+
+    return newUser;
   }
 
   public getUserView(
     id: string,
   ): Readonly<Pick<UserAccount, 'id' | 'email' | 'profile'>> | undefined {
-    return undefined;
+    const user = this.users[id];
+
+    if (!user) {
+      return undefined;
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      profile: user.profile,
+    };
   }
 }
