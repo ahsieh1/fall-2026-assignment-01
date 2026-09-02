06710a666422f3d0951083153514930b2d0b852f
ahsieh1 <ahsieh1@charlotte.edu>
2026-09-02 21:10:14 +0000

    Finished Excercise 14

--- src/exercise14.ts
+++ src/exercise14.ts
@@ -1,5 +1,17 @@
-export type PostItem = {};
+export type PostItem = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 export async function fetchPostBatch(postIds: number[]): Promise<PostItem[]> {
-  return [];
+  const fetchPromises = postIds.map(async (id) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/${id}',);
+    const post = (await response.json()) as PostItem;
+    return post;
+  });
+  
+  const posts = await Promise.all(fetchPromises);
+
+  return posts;
 }
