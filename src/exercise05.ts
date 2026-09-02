8a7b07e6b4b6721d8cc3f9ef8870d24b2fa0ebfc
ahsieh1 <ahsieh1@charlotte.edu>
2026-09-02 13:49:00 +0000

    Finished Excercise 5

--- src/exercise05.ts
+++ src/exercise05.ts
@@ -1,9 +1,22 @@
-export type NetworkConfig = {};
+export type NetworkConfig = {
+  serverUrl: string;
+  port: number;
+};
 
-export type EnvironmentConfig = {};
+export type EnvironmentConfig = {
+  environment: 'dev' | 'prod';
+  timeout: number;
+};
 
 export type AppConfig = NetworkConfig & EnvironmentConfig;
 
 export function initializeConfig(userOverrides: Partial<AppConfig>): AppConfig {
-  return {};
+  const defaultConfig: AppConfig = {
+    serverUrl: 'localhost',
+    port: 8080,
+    environment: 'dev',
+    timeout: 5000,
+  };
+
+  return { ...defaultConfig, ...userOverrides };
 }
