a7cf83d1cf5a0be5e0d2835cc6dbfc952f551d61
ahsieh1 <ahsieh1@charlotte.edu>
2026-09-02 14:33:17 +0000

    Finished Exercise 7

--- src/exercise07.ts
+++ src/exercise07.ts
@@ -1,3 +1,26 @@
+import fs from 'fs';
+
+export type StudentGrades = {
+    [subject: string]: number;
+  };
+
+  export type Gradebook = {
+    [studentName: string]: StudentGrades;
+  };
+
 export function calculateSubjectAverage(subject: string): number {
-  return 0;
+  const fileContent = fs.readFileSync('data/gradebook.json', 'utf-8');
+  const gradebook: Gradebook = JSON.parse(fileContent);
+
+  let totalScore = 0;
+  let studentCount = 0;
+
+  for (const studentGrades of Object.values(gradebook)) {
+    if (subject in studentGrades) {
+      totalScore += studentGrades[subject];
+      studentCount++;
+    }
+  }
+
+  return studentCount === 0 ? 0 : totalScore / studentCount;
 }
